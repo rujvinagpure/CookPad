72982b782a8cd7fef4cb47e96d86583ec2fe4cf9
Rujvi <nagpurerujvi1201@gmail.com>
2020-10-02 20:40:05 +0530

    ret

---
+++ app/src/main/java/com/example/cookpad/BaseModel.kt
@@ -0,0 +1,5 @@
+package com.example.cookpad
+
+import java.io.Serializable
+
+interface BaseModel : Serializable
---
+++ app/src/main/java/com/example/cookpad/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.cookpad
+
+class Constants {
+    companion object {
+        const val API_ENDPOINT = "https://www.themealdb.com/api/json/v1/1/categories.php"
+    }
+}
